37d8bdac9915e5f95ade6badf65c7330e0271422
pablo2389 <160980965+pablo2389@users.noreply.github.com>
2025-08-03 22:03:14 -0300

    Agrego script start y puerto dinámico

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
-app.get('/api/saludo', (req, res) => {
-  res.json({ mensaje: 'Hola desde el backend Node.js!' });
+app.get('/', (req, res) => {
+  res.json({ mensaje: 'Hola desde backend en Render!' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
