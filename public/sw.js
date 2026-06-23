88c268a58a03cd61089a6352a6fbd732b44a2428
pablo2389 <160980965+pablo2389@users.noreply.github.com>
2026-06-22 22:10:14 -0300

    PWA instalable

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'lavadero-v1';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
